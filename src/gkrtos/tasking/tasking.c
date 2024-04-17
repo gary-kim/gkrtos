185e69862537cd37779446d704a2f0c96afc42fb
Gary Kim <gary@garykim.dev>
2024-04-16 21:36:29 -0400

    enh: progress on tasking

---
+++ src/gkrtos/tasking/tasking.c
@@ -0,0 +1,16 @@
+#include "tasking.h"
+#include "stdint-gcc.h"
+
+struct gkrtos_tasking_task* gkrtos_tasking_task_new(
+    enum gkrtos_tasking_priority priority) {
+  unsigned static int current_max_pid = 0;
+  struct gkrtos_tasking_task* task = malloc(sizeof (struct gkrtos_tasking_task));
+  task->pid = current_max_pid++;
+  task->priority = priority;
+  task->current_priority = task->priority;
+  return task;
+}
+
+enum gkrtos_tasking_priority gkrtos_tasking_priority_user(uint8_t priority) {
+  return GKRTOS_TASKING_PRIORITY_USER + priority;
+}
--- src/gkrtos/tasking/tasking.h
+++ src/gkrtos/tasking/tasking.h
@@ -1,8 +1,32 @@
 #ifndef GKRTOS_TASKING_H
 #define GKRTOS_TASKING_H
 
+enum gkrtos_tasking_priority {
+  GKRTOS_TASKING_PRIORITY_REALTIME,
+  GKRTOS_TASKING_PRIORITY_KERNEL,
+  GKRTOS_TASKING_PRIORITY_PENDSV,
+  GKRTOS_TASKING_PRIORITY_USER,
+};
+
 struct gkrtos_tasking_task {
   unsigned int pid;
+
+  // priority is the real priority of the task. This should not change
+  // throughout the lifetime of the process.
+  enum gkrtos_tasking_priority priority;
+
+  // current_priority is the priority of the process at this moment. This may
+  // change in scenarios such as priority inversion.
+  enum gkrtos_tasking_priority current_priority;
 };
+
+// ===========================
+// === OS Public Functions ===
+// ===========================
+
+struct gkrtos_tasking_task* gkrtos_tasking_task_new(
+    enum gkrtos_tasking_priority priority);
+
+enum gkrtos_tasking_priority gkrtos_tasking_priority_user(uint8_t priority);
 
 #endif
