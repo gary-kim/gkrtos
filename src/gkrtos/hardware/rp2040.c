gary-kim/gkrtos

commit 6d8b014908ab2304485b4deedff5cd0fa36e1ad9

enh: set up systick with a configurable timing
Implements: https://todo.sr.ht/~gary-kim/gkrtos/4

src/gkrtos/hardware/rp2040.c
@@ -15,6 +15,7 @@
 
 #include "rp2040.h"
 
+#include "gkrtos/config.h"
 #include "hardware/clocks.h"
 #include "hardware/structs/sio.h"
 #include "hardware/sync.h"
@@ -22,3 +23,7 @@
 uint32_t gkrtos_get_cpuid() { return sio_hw->cpuid; }
 
 uint32_t gkrtos_get_cpu_frequency() { return clock_get_hz(clk_sys); }
+
+uint32_t gkrtos_get_systick_rvr_value() {
+  return gkrtos_get_cpu_frequency() / GKRTOS_SYSTICK_FREQUENCY_HZ
+}

src/gkrtos/config.h
@@ -17,6 +17,7 @@
 #define GKRTOS_CONFIG_H
 
 #define GKRTOS_CONFIG_MAX_TASKS 128
-#define GKRTOS_TASKING_TASK_ELAPSED_TIME_US (5 * 1000)
+#define GKRTOS_TASKING_TASK_ELAPSED_TIME_US (6 * 1000)
+#define GKRTOS_SYSTICK_FREQUENCY_HZ 1000
 
 #endif

src/gkrtos/hardware/rp2040.h
@@ -24,3 +24,5 @@ typedef void* gkrtos_stackptr_t;
 uint32_t gkrtos_get_cpuid();
 
 uint32_t gkrtos_get_cpu_frequency();
+
+uint32_t gkrtos_get_systick_rvr_value();

src/gkrtos/interrupts/systick.c
@@ -16,6 +16,7 @@
 #include "systick.h"
 
 #include "gkrtos/asm.h"
+#include "gkrtos/hardware/rp2040.h"
 #include "gkrtos/misc/misc.h"
 #include "hardware/exception.h"
 #include "hardware/regs/m0plus.h"
@@ -39,9 +40,12 @@ enum gkrtos_result gkrtos_init_systick_handler() {
   // https://datasheets.raspberrypi.com/rp2040/rp2040-datasheet.pdf#tab-registerlist_m0plus
   systick_hw->csr = ((0x1) |       // ENABLE: enable
                      (0x1 << 1) |  // TICKINT: Enable SysTick Exceptions
-                     (0x1 << 2)    // CLKSOURCE: Processor Clockj
+                     (0x1 << 2)    // CLKSOURCE: Processor Clock
   );
-  // TODO: Determine a more appropriate value for this.
+
+  // Make sure the value isn't so large that it can't be used
+  assert(gkrtos_get_systick_rvr_value() < 0x00FFFFFF);
+
   systick_hw->rvr = 0x00FFFFFF;
 
   // Set system handler priority

src/gkrtos/tasking/tasking.h
@@ -79,6 +79,14 @@ struct gkrtos_tasking_task {
   // TODO: Actually use this. Currently unused for simplicity
   gkrtos_stackptr_t stackbase;
 
+  // next_run_time contains the time at which this task must be queued next.
+  // This value is set to zero to indicate that there is no timing
+  // requirement. If the function must be run by a specific time, this will
+  // be set to the time at which it must be run. Then, on the systick before
+  // this time is reached, the task will be context switched to.
+  // TODO: Actually set this up
+  uint64_t next_run_time;
+
   struct {
     uint64_t ctx_switch_time;
     uint64_t run_ticks;

commit 6ff5828519fb22ffbc63c8492a1f8a1061f3feb8

fix: build failing
Fixes: 6d8b014908ab ("enh: set up systick with a configurable timing")

src/gkrtos/hardware/rp2040.c
@@ -25,5 +25,5 @@ uint32_t gkrtos_get_cpuid() { return sio_hw->cpuid; }
 uint32_t gkrtos_get_cpu_frequency() { return clock_get_hz(clk_sys); }
 
 uint32_t gkrtos_get_systick_rvr_value() {
-  return gkrtos_get_cpu_frequency() / GKRTOS_SYSTICK_FREQUENCY_HZ
+  return gkrtos_get_cpu_frequency() / GKRTOS_SYSTICK_FREQUENCY_HZ;
 }